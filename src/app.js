d8bd25fbef70898e629c14492845a206fefbef67
William Franca <macedo.franca@gmail.com>
2021-01-07 14:13:27 -0300

    Desafio conceitos de nodejs completo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid, v4 } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,55 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const newRepository = {id: v4(), title, url, techs, likes: 0};
+  repositories.push(newRepository);
+  return response.status(201).json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex(r => r.id === id);
+
+  if(index === -1) {
+    return response.status(400).json({error: 'Repository not found.'})
+  }
+
+  repositories[index].title = title;
+  repositories[index].url = url;
+  repositories[index].techs = techs;
+
+  return response.json(repositories[index]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(r => r.id === id);
+
+  if(index === -1) {
+    return response.status(400).json({error: 'Repository not found.'})
+  } else {
+    repositories.splice(index, 1);
+    return response.status(204).send();
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(r => r.id === id);
+
+  if(index === -1) {
+    return response.status(400).json({error: 'Repository not found.'})
+  } else {
+    repositories[index].likes = ++repositories[index].likes;
+    return response.json(repositories[index]);
+  }
 });
 
 module.exports = app;
